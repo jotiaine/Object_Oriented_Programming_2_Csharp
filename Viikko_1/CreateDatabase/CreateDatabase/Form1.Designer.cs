14c6e36754c3f8959dbfca8df821fb3f29e6e6e0
Joni Tiainen <jonitia91@gmail.com>
2022-09-08 11:56:13 +0300

    OOP 2 - first week initial files

---
+++ Viikko_1/CreateDatabase/CreateDatabase/Form1.Designer.cs
@@ -0,0 +1,71 @@
+﻿namespace CreateDatabase
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button2 = new System.Windows.Forms.Button();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(495, 181);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(238, 144);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 21);
+            this.comboBox2.TabIndex = 1;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(874, 513);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.button2);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ComboBox comboBox2;
+    }
+}
+
---
+++ Viikko_1/CreateDatabase/CreateDatabase/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SQLite;
+
+namespace CreateDatabase
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SQLiteConnection sqlite_conn;
+            sqlite_conn = CreateConnection();
+
+        }
+
+        static SQLiteConnection CreateConnection() 
+        {
+            SQLiteConnection sqlite_conn;
+            sqlite_conn = new SQLiteConnection("Data Source=customer.db; Version=3; New=True; Compress=True");
+            sqlite_conn.Open();
+
+            MessageBox.Show("Database created succesfully");
+
+            return sqlite_conn;
+        }
+
+
+        private void frmDatabase_Load (object sender, EventArgs e)
+        {
+            SQLiteConnection sqlite_conn;
+
+            sqlite_conn = new SQLiteConnection("Data Source=customer.db; Version=3; New=False; Compress=True");
+            sqlite_conn.Open();
+
+            SQLiteDataReader sqlite_datareader;
+            SQLiteCommand sqlite_cmd;
+            sqlite_cmd = sqlite_conn.CreateCommand();
+
+            sqlite_cmd.CommandText = "SELECT * FROM customer ORDER BY lastname";
+
+            sqlite_datareader = sqlite_cmd.ExecuteReader();
+
+            DataTable dt = new DataTable();
+            dt.Load(sqlite_datareader);
+
+            for (int i = 0; i < dt.Rows.Count; i++)
+            {
+                DataRow row = dt.Rows[i];
+                cmbCustomers.Items.Add(row["customerID"] + " " + row["lastname"] + row["firstname"] + row["email"]);
+            }
+
+            sqlite_conn.Close();
+        }
+
+        
+    }
+}
